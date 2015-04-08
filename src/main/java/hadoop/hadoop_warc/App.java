ba500b29187b92af87852d2654b5d524a5d7511f
zhuxf0407 <zhuxf0407@live.cn>
2015-04-09 01:10:52 +0200

    first commit

---
+++ src/main/java/hadoop/hadoop_warc/App.java
@@ -0,0 +1,13 @@
+package hadoop.hadoop_warc;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
